0737c65bd9dd2a2f0a132fc33888e94b11d91783
Yutong Wang <aeo9539@gmail.com>
2024-08-16 00:53:54 -0400

    [Asset] changed website icon

--- src/components/Chronology.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-function Chronology() {
-  const [calendars, setCalendars] = useState<string[]>([]);
-  const [yearStart, setYearStart] = useState<string>('');
-  const [yearEnd, setYearEnd] = useState<string>('');
-}
---
+++ src/components/LandingSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Landing(): React.ReactElement {
+  return (
+    <section className="landing">
+      <img
+        src="/frontpage-img.webp"
+        alt="frontpage image"
+        className="landing-image"
+      />
+    </section>
+  );
+}
+
+export default Landing;
