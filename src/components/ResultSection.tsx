ae82ef8c849d791e9643728f4c0a005d308f94a0
Yutong Wang <aeo9539@gmail.com>
2024-08-22 22:38:29 -0400

    [main] input section

---
+++ src/components/ResultSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface ResultProps {
+  calendars: string[];
+  years: number[];
+}
+
+function ResultSection({ calendars, years }: ResultProps) {
+  return null;
+}
+
+export default ResultSection;
