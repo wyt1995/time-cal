ea4ea77d2e7ede7c157b2319be19d0d16c6b23d5
Yutong Wang <aeo9539@gmail.com>
2024-09-14 23:33:27 -0400

    [util] sexagenary cycle array

---
+++ src/Cal/sexagenary-cycle.ts
@@ -0,0 +1,306 @@
+interface SexagenaryName {
+  name: string;
+  name_ch: string;
+}
+
+export const SexagenaryCycle: Array<SexagenaryName> = [
+  {
+    name: "jia zi",
+    name_ch: "甲子",
+  },
+
+  {
+    name: "yi chou",
+    name_ch: "乙丑",
+  },
+
+  {
+    name: "bing yin",
+    name_ch: "丙寅",
+  },
+
+  {
+    name: "ding mao",
+    name_ch: "丁卯",
+  },
+
+  {
+    name: "wu chen",
+    name_ch: "戊辰",
+  },
+
+  {
+    name: "ji si",
+    name_ch: "己巳",
+  },
+
+  {
+    name: "geng wu",
+    name_ch: "庚午",
+  },
+
+  {
+    name: "xin wei",
+    name_ch: "辛未",
+  },
+
+  {
+    name: "ren shen",
+    name_ch: "壬申",
+  },
+
+  {
+    name: "gui you",
+    name_ch: "癸酉",
+  },
+
+  {
+    name: "jia xu",
+    name_ch: "甲子",
+  },
+
+  {
+    name: "yi hai",
+    name_ch: "甲戌",
+  },
+
+  {
+    name: "bing zi",
+    name_ch: "丙子",
+  },
+
+  {
+    name: "ding chou",
+    name_ch: "丁丑",
+  },
+
+  {
+    name: "wu yin",
+    name_ch: "戊寅",
+  },
+
+  {
+    name: "ji mao",
+    name_ch: "己卯",
+  },
+
+  {
+    name: "geng chen",
+    name_ch: "庚辰",
+  },
+
+  {
+    name: "xin si",
+    name_ch: "辛巳",
+  },
+
+  {
+    name: "ren wu",
+    name_ch: "壬午",
+  },
+
+  {
+    name: "gui wei",
+    name_ch: "癸未",
+  },
+
+  {
+    name: "jia shen",
+    name_ch: "甲申",
+  },
+
+  {
+    name: "yi you",
+    name_ch: "乙酉",
+  },
+
+  {
+    name: "bing xu",
+    name_ch: "丙戌",
+  },
+
+  {
+    name: "ding hai",
+    name_ch: "丁亥",
+  },
+
+  {
+    name: "wu zi",
+    name_ch: "戊子",
+  },
+
+  {
+    name: "ji chou",
+    name_ch: "己丑",
+  },
+
+  {
+    name: "geng yin",
+    name_ch: "庚寅",
+  },
+
+  {
+    name: "xin mao",
+    name_ch: "辛卯",
+  },
+
+  {
+    name: "ren chen",
+    name_ch: "壬辰",
+  },
+
+  {
+    name: "gui si",
+    name_ch: "癸巳",
+  },
+
+  {
+    name: "jia wu",
+    name_ch: "甲午",
+  },
+
+  {
+    name: "yi wei",
+    name_ch: "乙未",
+  },
+
+  {
+    name: "bing shen",
+    name_ch: "丙申",
+  },
+
+  {
+    name: "ding you",
+    name_ch: "丁酉",
+  },
+
+  {
+    name: "wu xu",
+    name_ch: "戊戌",
+  },
+
+  {
+    name: "ji hai",
+    name_ch: "己亥",
+  },
+
+  {
+    name: "geng zi",
+    name_ch: "甲子",
+  },
+
+  {
+    name: "xin chou",
+    name_ch: "辛丑",
+  },
+
+  {
+    name: "ren yin",
+    name_ch: "壬寅",
+  },
+
+  {
+    name: "gui mao",
+    name_ch: "癸卯",
+  },
+
+  {
+    name: "jia chen",
+    name_ch: "甲辰",
+  },
+
+  {
+    name: "yi si",
+    name_ch: "乙巳",
+  },
+
+  {
+    name: "bing wu",
+    name_ch: "丙午",
+  },
+
+  {
+    name: "ding wei",
+    name_ch: "丁未",
+  },
+
+  {
+    name: "wu shen",
+    name_ch: "戊申",
+  },
+
+  {
+    name: "ji you",
+    name_ch: "己酉",
+  },
+
+  {
+    name: "geng xu",
+    name_ch: "庚戌",
+  },
+
+  {
+    name: "xin hai",
+    name_ch: "辛亥",
+  },
+
+  {
+    name: "ren zi",
+    name_ch: "壬子",
+  },
+
+  {
+    name: "gui chou",
+    name_ch: "癸丑",
+  },
+
+  {
+    name: "jia yin",
+    name_ch: "甲寅",
+  },
+
+  {
+    name: "yi mao",
+    name_ch: "乙卯",
+  },
+
+  {
+    name: "bing chen",
+    name_ch: "丙辰",
+  },
+
+  {
+    name: "ding si",
+    name_ch: "丁巳",
+  },
+
+  {
+    name: "wu wu",
+    name_ch: "戊午",
+  },
+
+  {
+    name: "ji wei",
+    name_ch: "己未",
+  },
+
+  {
+    name: "geng shen",
+    name_ch: "庚申",
+  },
+
+  {
+    name: "xin you",
+    name_ch: "辛酉",
+  },
+
+  {
+    name: "ren xu",
+    name_ch: "壬戌",
+  },
+
+  {
+    name: "gui hai",
+    name_ch: "癸亥",
+  },
+];
